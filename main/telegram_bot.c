ad1e48eaff57d5293affd2d69ac1d2f2f4523223
Valeriy Chudnikov <memricpf@gmail.com>
2022-08-27 16:07:16 +0300

    simple echo test

--- main/telegram_bot.c
+++ main/telegram_bot.c
@@ -37,15 +37,21 @@ char url[256];
 char buf[512];
 char resp[512] = {0};
 
-static int32_t TeleBot_Get_Request(const char *http_mthd, const char *t_mthd, char *resp, uint32_t resp_len);
+static int32_t TeleBot_Http_Request(const char *http_mthd, const char *t_mthd,
+		char *req, uint32_t req_len,
+		char *resp, uint32_t resp_len);
 static void TeleBot_Task(void *arg);
+int32_t TeleBot_SendMessage(uint32_t chat_id, const char *msg);
+void TeleBot_MessageCallback(uint32_t chat_id, const char *msg);
 
 void TeleBot_Init(void)
 {
 	xTaskCreate(TeleBot_Task, "Telegram Bot", 1024*6, NULL, 3, &telebot_task);
 }
 
-static int32_t TeleBot_Get_Request(const char *http_mthd, const char *t_mthd, char *resp, uint32_t resp_len)
+static int32_t TeleBot_Http_Request(const char *http_mthd, const char *t_mthd,
+		char *req, uint32_t req_len,
+		char *resp, uint32_t resp_len)
 {
     int ret, len;
     int32_t ret_len = 0;
@@ -80,12 +86,27 @@ static int32_t TeleBot_Get_Request(const char *http_mthd, const char *t_mthd, ch
         /*Compose request*/
         len = snprintf(buf, sizeof(buf), "%s /bot%s/%s HTTP/1.1\r\n"
         		"Host: "WEB_SERVER"\r\n"
-//                "User-Agent: esp-idf/1.0 esp32\r\n"
-				"Connection: close\r\n"
-				"\r\n",
+                /*"User-Agent: esp-idf/1.0 esp32\r\n"*/
+				"Connection: close\r\n",
+//				"\r\n",
 				http_mthd, BOT_TOKEN, t_mthd);
 
         if (len < 0) return -1;
+
+        if ((req != NULL) && (req_len < (sizeof(buf) - len)))
+        {
+        	len += snprintf(&buf[len], sizeof(buf) - len,
+        			"Content-Type: application/json\r\n"
+        			"Content-Length: %d\r\n\r\n", req_len); //TODO Check length
+
+        	len += snprintf(&buf[len], sizeof(buf) - len, "%s", req);
+//        	memcpy(&buf[len], req, req_len);
+//        	len += req_len;
+        }
+        else
+        {
+        	len += snprintf(&buf[len], sizeof(buf) - len, "\r\n");
+        }
 
         ESP_LOGD(TAG, "HTTP Request: %s", buf);
 
@@ -169,35 +190,106 @@ static int32_t TeleBot_Get_Request(const char *http_mthd, const char *t_mthd, ch
     return ret_len;
 }
 
-static void TeleBot_GetUpdates(void)
+/**
+ * getUpdates method
+ */
+static int32_t TeleBot_GetUpdates(int32_t *id)
 {
-	int32_t rx_len = TeleBot_Get_Request("GET", "getUpdates", resp, sizeof(resp));
+	int32_t retval = -1;
 
-	if (rx_len > 0)
+	/*Construct JSON method object*/
+	cJSON *message = cJSON_CreateObject();
+
+	if (message != NULL)
 	{
-		ESP_LOGD(TAG, "Resp: %s", resp);
+		cJSON *msg_timeout = cJSON_CreateNumber(5);
+		cJSON *msg_offset = cJSON_CreateNumber(*id);
 
-		cJSON *json = cJSON_ParseWithLength(resp, sizeof(resp));
-
-		if (json != NULL)
+		if (msg_timeout != NULL && msg_offset != NULL)
 		{
-			cJSON *result = cJSON_GetObjectItemCaseSensitive(json, "result");
-			cJSON *res_item;
-			cJSON_ArrayForEach(res_item, result)
+			cJSON_AddItemToObject(message, "timeout", msg_timeout);
+			cJSON_AddItemToObject(message, "offset", msg_offset);
+
+			char *req = cJSON_PrintUnformatted(message);
+
+			retval = TeleBot_Http_Request("POST", "getUpdates", req, strlen(req), resp, sizeof(resp));
+
+			if (retval > 0)
 			{
-				cJSON *message = cJSON_GetObjectItemCaseSensitive(res_item, "message");
-				cJSON *text = cJSON_GetObjectItemCaseSensitive(message, "text");
+				ESP_LOGD(TAG, "Resp: %s", resp);
 
-				if (text != NULL)
+				cJSON *json = cJSON_ParseWithLength(resp, sizeof(resp));
+
+				if (json != NULL)
 				{
-					cJSON *chat = cJSON_GetObjectItemCaseSensitive(message, "chat");
-					cJSON *chat_id = cJSON_GetObjectItemCaseSensitive(chat, "id");
+					cJSON *result = cJSON_GetObjectItemCaseSensitive(json, "result");
+					cJSON *res_item;
+					cJSON_ArrayForEach(res_item, result)
+					{
+						/*Get update id*/
+						cJSON *upd_id = cJSON_GetObjectItemCaseSensitive(res_item, "update_id"); //TODO Check NULL
+						int32_t updt_id_val = upd_id->valueint;
+						if (updt_id_val >= *id)
+						{
+							*id = updt_id_val + 1;
+						}
 
-					ESP_LOGI(TAG, "Message from %d: %s", chat_id->valueint, text->valuestring);
+						cJSON *message = cJSON_GetObjectItemCaseSensitive(res_item, "message");
+						cJSON *text = cJSON_GetObjectItemCaseSensitive(message, "text");
+
+						if (text != NULL)
+						{
+							cJSON *chat = cJSON_GetObjectItemCaseSensitive(message, "chat");
+							cJSON *chat_id = cJSON_GetObjectItemCaseSensitive(chat, "id");
+
+							TeleBot_MessageCallback((uint32_t) chat_id->valueint, text->valuestring);
+						}
+					}
+
+					cJSON_Delete(json);
 				}
 			}
+
+			free(req);
 		}
+
+		cJSON_Delete(message);
 	}
+
+	return retval;
+}
+
+/**
+ * Sends message
+ */
+int32_t TeleBot_SendMessage(uint32_t chat_id, const char *msg)
+{
+	int32_t retval = -1;
+
+	/*Construct JSON method object*/
+	cJSON *message = cJSON_CreateObject();
+
+	if (message != NULL)
+	{
+		cJSON *msg_chat_id = cJSON_CreateNumber(chat_id);
+		cJSON *msg_text = cJSON_CreateString(msg);
+
+		if (msg_chat_id != NULL && msg_text != NULL)
+		{
+			cJSON_AddItemToObject(message, "chat_id", msg_chat_id);
+			cJSON_AddItemToObject(message, "text", msg_text);
+
+			char *req = cJSON_PrintUnformatted(message);
+
+			retval = TeleBot_Http_Request("POST", "sendMessage", req, strlen(req), resp, sizeof(resp));
+
+			free(req);
+		}
+
+		cJSON_Delete(message);
+	}
+
+	return retval;
 }
 
 /**
@@ -206,10 +298,28 @@ static void TeleBot_GetUpdates(void)
 static void TeleBot_Task(void *arg)
 {
 	(void) arg;
+	int32_t id = -1;
+	int32_t resp_len;
 
 	while (1)
 	{
-		TeleBot_GetUpdates();
+		resp_len = TeleBot_GetUpdates(&id);
+
+		if (resp_len > 0 && id != -1)
+		{
+		}
+
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
+}
+
+/**
+ * Message callback
+ */
+void TeleBot_MessageCallback(uint32_t chat_id, const char *msg)
+{
+	ESP_LOGI(TAG, "Message from %d: %s", chat_id, msg);
+
+	/*MEssage echo*/
+	TeleBot_SendMessage(chat_id, msg);
 }
